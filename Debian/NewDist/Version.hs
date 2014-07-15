347190a2a9f8452a0e758b260ac5462091fe9c37
David Fox <dsf@seereason.com>
2014-07-15 14:59:26 +0000

    Version 5.65 - remove Cabal dependency

--- Debian/NewDist/Version.hs
+++ Debian/NewDist/Version.hs
@@ -2,22 +2,7 @@
 module Version (myVersion) where
 
 import Data.Version (showVersion)
-import Distribution.Simple.Utils (findPackageDesc)
-import Distribution.Package (pkgVersion)
-import Distribution.PackageDescription (package, packageDescription)
-import Distribution.PackageDescription.Parse (readPackageDescription)
-import Distribution.Verbosity (silent)
-import Language.Haskell.TH (runIO)
-import Language.Haskell.TH.Syntax (lift)
+import Paths_debian_repo (version)
 
--- Compiles in the current version of the autobuilder by looking in the .cabal file.
--- To convert this to type Version, remove ". showVersion" from below.
 myVersion :: String
-myVersion = $(runIO (findPackageDesc "." >>=
-#if MIN_VERSION_Cabal(1,19,0)
-                              either error (readPackageDescription silent) >>=
-#else
-                              readPackageDescription silent >>=
-#endif
-                              return . pkgVersion . package . packageDescription) >>=
-                       lift . showVersion)
+myVersion = showVersion version
