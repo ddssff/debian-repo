06a53916b73141139676ae0a5e09135bb439b2dd
David Fox <dsf@seereason.com>
2014-08-25 16:40:50 +0000

    Improved rsync error handling

--- Debian/Repo/Prelude.hs
+++ Debian/Repo/Prelude.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE FlexibleContexts, FlexibleInstances, GeneralizedNewtypeDeriving, MultiParamTypeClasses, OverloadedStrings,
+{-# LANGUAGE CPP, FlexibleContexts, FlexibleInstances, GeneralizedNewtypeDeriving, MultiParamTypeClasses, OverloadedStrings,
              PackageImports, ScopedTypeVariables, TemplateHaskell, TypeSynonymInstances, UndecidableInstances #-}
 {-# OPTIONS_GHC -Wall -fno-warn-orphans #-}
 module Debian.Repo.Prelude
@@ -98,6 +98,59 @@ rsync extra source dest =
        case result of
          [x] -> return x
          _ -> error "Missing or multiple exit codes"
+
+#if 0
+data RsyncError
+    = RsyncSuccess
+    | RsyncSyntaxOrUsage
+    | RsyncProtocolIncompatibility
+    | RsyncErrorsSelectingInputOutputFiles
+    | RsyncRequestedActionNotSupported
+    | RsyncErrorStartingClientServerProtocol
+    | RsyncDaemonUnableToAppendToLogfile
+    | RsyncErrorInSocketIO
+    | RsyncErrorInFileIO
+    | RsyncErrorInRsyncProtocolDataStream
+    | RsyncErrorsWithProgramDiagnostics
+    | RsyncErrorInIPCCode
+    | RsyncReceivedSIGUSR1orSIGINT
+    | RsyncSomeErrorReturnedByWaitpid
+    | RsyncErrorAllocatingCoreMemoryBuffers
+    | PartialTransferDueToError
+    | PartialTransferDueToVanishedSourceFiles
+    | TheMaxDeleteLimitStoppedDeletions
+    | TimeoutInDataSendReceive
+    | TimeoutWaitingForDaemonConnection
+    | RsyncUnexpected Int
+
+rsyncError :: Int -> RsyncError
+rsyncError = fst . rsyncErrorInfo
+
+rsyncErrorMessage :: Int -> RsyncError
+rsyncErrorMessage = snd . rsyncErrorInfo
+
+rsyncErrorInfo :: Int -> (RsyncError, String)
+rsyncErrorInfo 1  = (RsyncSyntaxOrUsage, "rsync: Syntax or usage error")
+rsyncErrorInfo 2  = (RsyncProtocolIncompatibility, "rsync: Protocol incompatibility")
+rsyncErrorInfo 3  = (RsyncErrorsSelectingInputOutputFiles, "rsync: Errors selecting input/output files, dirs")
+rsyncErrorInfo 4  = (RsyncRequestedActionNotSupported, "rsync: Requested action not supported: an attempt was made to manipulate 64-bit files on a platform that cannot support them; or an option was specified that is supported by the client and not by the server.")
+rsyncErrorInfo 5  = (RsyncErrorStartingClientServerProtocol, "rsync: Error starting client-server protocol")
+rsyncErrorInfo 6  = (RsyncDaemonUnableToAppendToLogfile, "rsync: Daemon unable to append to log-file")
+rsyncErrorInfo 10 = (RsyncErrorInSocketIO, "rsync: Error in socket I/O")
+rsyncErrorInfo 11 = (RsyncErrorInFileIO, "rsync: Error in file I/O")
+rsyncErrorInfo 12 = (RsyncErrorInRsyncProtocolDataStream, "rsync: Error in rsync protocol data stream")
+rsyncErrorInfo 13 = (RsyncErrorsWithProgramDiagnostics, "rsync: Errors with program diagnostics")
+rsyncErrorInfo 14 = (RsyncErrorInIPCCode, "rsync: Error in IPC code")
+rsyncErrorInfo 20 = (RsyncReceivedSIGUSR1orSIGINT, "rsync: Received SIGUSR1 or SIGINT")
+rsyncErrorInfo 21 = (RsyncSomeErrorReturnedByWaitpid, "rsync: Some error returned by waitpid()")
+rsyncErrorInfo 22 = (RsyncErrorAllocatingCoreMemoryBuffers, "rsync: Error allocating core memory buffers")
+rsyncErrorInfo 23 = (PartialTransferDueToError, "Partial transfer due to error")
+rsyncErrorInfo 24 = (PartialTransferDueToVanishedSourceFiles, "Partial transfer due to vanished source files")
+rsyncErrorInfo 25 = (TheMaxDeleteLimitStoppedDeletions, "rsync: The --max-delete limit stopped deletions")
+rsyncErrorInfo 30 = (TimeoutInDataSendReceive, "rsync: Timeout in data send/receive")
+rsyncErrorInfo 35 = (TimeoutWaitingForDaemonConnection, "rsync: Timeout waiting for daemon connection")
+rsyncErrorInfo n = (RsyncUnexpected n, "Unexpected rsync error: " ++ show n)
+#endif
 
 checkRsyncExitCode :: Monad m => ExitCode -> m ()
 checkRsyncExitCode ExitSuccess = return ()
